1e272ed3ed603a76a3b0e8f02dc104a7f2129c79
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2024-11-12 22:10:56 +0100

    idk

--- backend/models.py
+++ backend/models.py
@@ -7,3 +7,10 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 
 from django.db import models
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+
