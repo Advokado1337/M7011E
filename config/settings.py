0e67d4a42e1f6c812147d04c92cee0fea55f0ac1
Ludvig Järvi <jarviludvig@icloud.com>
2024-11-13 11:46:51 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'backend' / 'templates'],  # Add templates directory to the TEMPLATES setting
+        'DIRS': [BASE_DIR / 'app' / 'templates'],  # Add templates directory to the TEMPLATES setting
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
