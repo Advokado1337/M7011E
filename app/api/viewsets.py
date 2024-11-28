25ccd3a95707b2176ff1e1b745bc66eae0c8bf6a
Anton Follinger <47656465+Hundmat@users.noreply.github.com>
2024-11-28 16:31:31 +0100

    new post for user

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -80,7 +80,9 @@ def users_detail(request, pk):
             return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Category CRUD operations
-@api_view(['GET', 'POST'])
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@token_and_superuser_required
 def category_list(request):
     if request.method == 'GET':
         categories = Category.objects.all()
@@ -97,25 +99,19 @@ def category_list(request):
             return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def category_detail(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = CategorySerializer(category, data=request.data)
+        serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            category = serializer.save()
+            response_data = {
+                'message': 'Category updated successfully!',
+                'data': serializer.data,
+            }
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
     elif request.method == 'DELETE':
         category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT) 
 
 # Movie CRUD operations
 @api_view(['GET', 'POST'])
--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'rest_framework.authtoken'
-
 ]
 
 
