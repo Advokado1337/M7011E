d553c08b9cac8b68850a6492c7620f6593445c47
Anton Follinger <47656465+Hundmat@users.noreply.github.com>
2024-12-11 14:33:02 +0100

    fixed decorator

--- app/decorators.py
+++ app/decorators.py
@@ -4,33 +4,28 @@ from rest_framework.authtoken.models import Token
 
 
 def token_and_superuser_required(view_func):
-    """
-    A decorator to check if the user has a valid token and is a superuser.
-    """
     @wraps(view_func)
-    def _wrapped_view(request, *args, **kwargs):
-        # Extract the token from the Authorization header
+    def _wrapped_view(self, request, *args, **kwargs):
+        # Check Authorization header
+        print(request.headers)
         auth_header = request.headers.get('Authorization')
-
-        print(f"Authorization Header: {auth_header}")
         if not auth_header or not auth_header.startswith('Token '):
             return JsonResponse({'error': 'Token required'}, status=401)
-
+        
         token_key = auth_header.split('Token ')[1]
         try:
-            # Validate the token and get the associated user
             token = Token.objects.get(key=token_key)
-            user = token.user  # Get the user linked to the token
-            request.user = user  # Attach the user to the request object
+            user = token.user
         except Token.DoesNotExist:
             return JsonResponse({'error': 'Invalid token'}, status=401)
-
+        
         # Check if the user is a superuser
         if not user.is_superuser:
             return JsonResponse({'error': 'Superuser access required'}, status=403)
-
-        # Proceed to the view
-        return view_func(request, *args, **kwargs)
+        
+        # Allow the original view to execute
+        return view_func(self, request, *args, **kwargs)
+    
     return _wrapped_view
 
 def token_and_isstaff_required(view_func):
